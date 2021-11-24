10d497e3ced79d353518e54738fcec98fec4cf18
James Butcher <jamesbutcher167@gmail.com>
2021-11-24 21:16:22 +0000

    Add projects endpoint

---
+++ pages/projects.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Projects: NextPage = () => {
+    return (
+        <Image className="rounded-xl flex" width="400" height="200" src="https://opengraph.githubassets.com/f46d3d21c0c43e565e27e314eb51c91b17f78342c1c4de976401d50a39d1a674/KJ002/Portfolio"></Image>
+    )
+}
+
+export default Projects
