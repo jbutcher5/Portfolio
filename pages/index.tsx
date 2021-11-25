4aba5e6f8c2652120a302b1c0bcc4bd6918b040a
James Butcher <jamesbutcher167@gmail.com>
2021-11-25 22:58:59 +0000

    Add href to github.com/KJ002

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,9 @@ const Home: NextPage = () => {
   return (
     <div className="grid place-items-center h-screen">
       <div className="p-6 max-w-sm mx-auto bg-nord5 rounded-xl shadow-md space-x-4 h-48 ring ring-nord9">
-        <Image className='fixed top-1 rounded-full' width='64' height='64' src='https://avatars.githubusercontent.com/u/36408549?s=64&v=4'></Image>
+        <a href='https://github.com/KJ002'>
+          <Image className='fixed top-1 rounded-full' width='64' height='64' src='https://avatars.githubusercontent.com/u/36408549?s=64&v=4'></Image>
+        </a>
         <div className="text-xl font-medium text-nord1">James Butcher</div>
         <p className="text-gray-500">C/C++, Python, Nim developer</p>
         <a href="/projects">
