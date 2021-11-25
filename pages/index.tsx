c2d17ea7d02e7b658b345c4bd44168a05f2650e5
James Butcher <jamesbutcher167@gmail.com>
2021-11-25 21:45:17 +0000

    Add projects button

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,13 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div className="grid place-items-center h-screen">
-      <div className="p-6 max-w-sm mx-auto bg-nord5 rounded-xl shadow-md flex space-x-4 h-48 flex-wrap ring">
-        <div className="text-xl font-medium text-black">Portfolio</div>
+      <div className="p-6 max-w-sm mx-auto bg-nord5 rounded-xl shadow-md space-x-4 h-48 ring ring-nord9">
+        <Image className='fixed top-1 rounded-full' width='64' height='64' src='https://avatars.githubusercontent.com/u/36408549?s=64&v=4'></Image>
+        <div className="text-xl font-medium text-nord1">James Butcher</div>
         <p className="text-gray-500">C/C++, Python, Nim developer</p>
+        <a href="/projects">
+          <button className='p-1 ring-nord9 hover:ring bg-nord10 rounded text-nord4'>Projects</button>
+        </a>
       </div>
     </div>
   )
