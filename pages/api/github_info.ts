b76083f468aacd54de2525f8fa76ffd1a68b3aa1
James Butcher <jamesbutcher167@gmail.com>
2021-11-26 21:23:12 +0000

    Fetch KJ002 github profile data with cache

---
+++ pages/api/github_info.ts
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+var LastValidRequest: number = Date.now();
+
+var cache: object;
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<object>
+) {
+
+  if (Date.now()-LastValidRequest > 1000000 || cache === undefined){
+    LastValidRequest = Date.now();
+    fetch('https://api.github.com/users/KJ002')
+      .then(value => value.json())
+      .then(data => cache = data);
+
+    console.log(Date.now())
+  }
+
+  res.status(200).json(cache)
+}
