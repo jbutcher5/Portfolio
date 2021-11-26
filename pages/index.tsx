2a660a8dd828778f7f872533039f707d1b115b09
James Butcher <jamesbutcher167@gmail.com>
2021-11-26 18:22:08 +0000

    Add secrets support

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,15 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Octokit, App } from 'octokit'
+import secrets from '../secrets.json'
+
+const octokit = new Octokit();
 
 const Home: NextPage = () => {
+  //const getID = () => {octokit.request('GET /user')}
+  //getID()
+
   return (
     <div className="grid place-items-center h-screen">
       <div className="p-6 max-w-sm mx-auto bg-nord5 rounded-xl shadow-md space-x-4 h-48 ring ring-nord9">
@@ -11,7 +18,7 @@ const Home: NextPage = () => {
           <Image className='fixed top-1 rounded-full' width='64' height='64' src='https://avatars.githubusercontent.com/u/36408549?s=64&v=4'></Image>
         </a>
         <div className="text-xl font-medium text-nord1">James Butcher</div>
-        <p className="text-gray-500">C/C++, Python, Nim developer</p>
+        <p className="text-gray-500">{secrets["github-token"]}</p>
         <a href="/projects">
           <button className='p-1 ring-nord9 hover:ring bg-nord10 rounded text-nord4'>Projects</button>
         </a>
