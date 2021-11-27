52607e47595232ff894ffcb8b25258e8d4582f0a
James Butcher <jamesbutcher167@gmail.com>
2021-11-27 13:37:44 +0000

    Dynamically generate HTML for each project

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,39 +3,34 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+const viewProjects = [
+    "Portfolio",
+    "Tetris",
+    "Muon", 
+    "read-yaml", 
+    "MicroClock", 
+    "MathEvaluator", 
+    "Raymarching",
+    "Pong",
+    "Thunder",
+    "Neo"
+];
+
+function generateProjectHtml(name: string){
+    let link: string = "https://github.com/KJ002/"+name;
+    let image: string = "https://opengraph.githubassets.com/f46d3d21c0c43e565e27e314eb51c91b17f78342c1c4de976401d50a39d1a674/KJ002/"+name;
+
+    return (
+        <a className='rounded-md hover:shadow-xl' href={link}>
+            <Image className="rounded-md" width="400" height="200" src={image}></Image>
+        </a>
+    )
+}
+
 const Projects: NextPage = () => {
     return (
         <div className='p-10 flex flex-wrap h-screen w-screen content-start justify-center items-center gap-10'>
-            <a className='rounded-md hover:shadow-xl' href='https://github.com/KJ002/Portfolio'>
-                <Image className="rounded-md" width="400" height="200" src="https://opengraph.githubassets.com/f46d3d21c0c43e565e27e314eb51c91b17f78342c1c4de976401d50a39d1a674/KJ002/Portfolio"></Image>
-            </a>
-            <a className='rounded-md hover:shadow-xl' href='https://github.com/KJ002/Tetris'>
-                <Image className="rounded-md" width="400" height="200" src="https://opengraph.githubassets.com/f46d3d21c0c43e565e27e314eb51c91b17f78342c1c4de976401d50a39d1a674/KJ002/Tetris"></Image>
-            </a>
-            <a className='rounded-md hover:shadow-xl' href='https://github.com/KJ002/Muon'>
-                <Image className="rounded-md" width="400" height="200" src="https://opengraph.githubassets.com/f46d3d21c0c43e565e27e314eb51c91b17f78342c1c4de976401d50a39d1a674/KJ002/Muon"></Image>
-            </a>
-            <a className='rounded-md hover:shadow-xl' href='https://github.com/KJ002/read-yaml'>
-                <Image className="rounded-md" width="400" height="200" src="https://opengraph.githubassets.com/f46d3d21c0c43e565e27e314eb51c91b17f78342c1c4de976401d50a39d1a674/KJ002/read-yaml"></Image>
-            </a>
-            <a className='rounded-md hover:shadow-xl' href='https://github.com/KJ002/MicroClock'>
-                <Image className="rounded-md" width="400" height="200" src="https://opengraph.githubassets.com/f46d3d21c0c43e565e27e314eb51c91b17f78342c1c4de976401d50a39d1a674/KJ002/MicroClock"></Image>
-            </a>
-            <a className='rounded-md hover:shadow-xl' href='https://github.com/KJ002/MathEvaluator'>
-                <Image className="rounded-md" width="400" height="200" src="https://opengraph.githubassets.com/f46d3d21c0c43e565e27e314eb51c91b17f78342c1c4de976401d50a39d1a674/KJ002/MathEvaluator"></Image>
-            </a>
-            <a className='rounded-md hover:shadow-xl' href='https://github.com/KJ002/Raymarching'>
-                <Image className="rounded-md" width="400" height="200" src="https://opengraph.githubassets.com/f46d3d21c0c43e565e27e314eb51c91b17f78342c1c4de976401d50a39d1a674/KJ002/Raymarching"></Image>
-            </a>
-            <a className='rounded-md hover:shadow-xl' href='https://github.com/KJ002/Pong'>
-                <Image className="rounded-md" width="400" height="200" src="https://opengraph.githubassets.com/f46d3d21c0c43e565e27e314eb51c91b17f78342c1c4de976401d50a39d1a674/KJ002/Pong"></Image>
-            </a>
-            <a className='rounded-md hover:shadow-xl' href='https://github.com/KJ002/Thunder'>
-                <Image className="rounded-md" width="400" height="200" src="https://opengraph.githubassets.com/f46d3d21c0c43e565e27e314eb51c91b17f78342c1c4de976401d50a39d1a674/KJ002/Thunder"></Image>
-            </a>
-            <a className='rounded-md hover:shadow-xl' href='https://github.com/KJ002/Neo'>
-                <Image className="rounded-md" width="400" height="200" src="https://opengraph.githubassets.com/f46d3d21c0c43e565e27e314eb51c91b17f78342c1c4de976401d50a39d1a674/KJ002/Neo"></Image>
-            </a>
+            {viewProjects.map(projectName => generateProjectHtml(projectName))}
         </div>
     )
 }
