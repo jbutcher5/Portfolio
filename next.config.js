4bc73d7522a256b4fc13eaa905f766afeba04343
James Butcher <jamesbutcher167@gmail.com>
2021-11-27 17:41:26 +0000

    Remove aws packages

--- next.config.js
+++ next.config.js
@@ -5,26 +5,4 @@ module.exports = {
     domains: ['opengraph.githubassets.com', 'avatars.githubusercontent.com'],
     formats: ['image/avif', 'image/webp']
   },
-/*  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block'
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff'
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'"
-          }          
-        ],
-      },
-    ]
-  },*/
 }
