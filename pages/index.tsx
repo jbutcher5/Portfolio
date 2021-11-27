c54231ffef449aad232d85e48832a25b889ddde3
James Butcher <jamesbutcher167@gmail.com>
2021-11-27 12:30:52 +0000

    Add getStaticProps and pump data to Home function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,34 @@
+import { data } from 'autoprefixer';
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { Octokit, App } from 'octokit'
-import secrets from '../secrets.json'
 
-const octokit = new Octokit();
+export const getStaticProps = async () => {
 
-const Home: NextPage = () => {
-  //const getID = () => {octokit.request('GET /user')}
-  //getID()
+  const response = await fetch('https://api.github.com/users/KJ002')
+  const data = await response.json();
+
+  
+  return {
+    props: { GithubInfo: data }
+  };
+
+}
+
+const Home: NextPage = ({GithubInfo}) => {
+
+  let githubProfile = 'https://github.com/'+GithubInfo["login"];
+  let profilePicture = 'https://avatars.githubusercontent.com/u/'+GithubInfo["id"].toString()+'?s=64&v=4';
 
   return (
     <div className="grid place-items-center h-screen">
       <div className="p-6 max-w-sm mx-auto bg-nord5 rounded-xl shadow-md space-x-4 h-48 ring ring-nord9">
-        <a href='https://github.com/KJ002'>
-          <Image className='fixed top-1 rounded-full' width='64' height='64' src='https://avatars.githubusercontent.com/u/36408549?s=64&v=4'></Image>
+        <a href={githubProfile}>
+          <Image className='fixed top-1 rounded-full' width='64' height='64' src={profilePicture}></Image>
         </a>
-        <div className="text-xl font-medium text-nord1">James Butcher</div>
-        <p className="text-gray-500">{secrets["github-token"]}</p>
+        <div className="text-xl font-medium text-nord1">{GithubInfo["name"]}</div>
+        <p className="text-gray-500">{GithubInfo["bio"]}</p>
         <a href="/projects">
           <button className='p-1 ring-nord9 hover:ring bg-nord10 rounded text-nord4'>Projects</button>
         </a>
