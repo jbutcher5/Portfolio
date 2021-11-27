a296a747f81a3c5deec55901828abf0461fd4f93
James Butcher <jamesbutcher167@gmail.com>
2021-11-27 12:31:18 +0000

    Delete all api endpoints

--- next.config.js
+++ next.config.js
@@ -4,5 +4,27 @@ module.exports = {
   images: {
     domains: ['opengraph.githubassets.com', 'avatars.githubusercontent.com'],
     formats: ['image/avif', 'image/webp']
-  }
+  },
+/*  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'"
+          }          
+        ],
+      },
+    ]
+  },*/
 }
