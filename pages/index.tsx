54295112f5f1f7c81290547a3bd0427e3ace004c
James Butcher <jamesbutcher167@gmail.com>
2021-11-28 17:55:42 +0000

    Add title to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,16 +23,21 @@ const Home: NextPage = ({ GithubInfo }: any) => {
   let profilePicture = 'https://avatars.githubusercontent.com/u/'+GithubInfo["id"].toString()+'?s=64&v=4';
 
   return (
-    <div className="grid place-items-center h-screen">
-      <div className="p-6 max-w-sm mx-auto bg-nord5 rounded-xl shadow-md space-x-4 h-48 ring ring-nord9">
-        <a href={githubProfile}>
-          <Image className='fixed top-1 rounded-full' width='64' height='64' src={profilePicture}></Image>
-        </a>
-        <div className="text-xl font-medium text-nord1">{GithubInfo["name"]}</div>
-        <p className="text-gray-500">{GithubInfo["bio"]}</p>
-        <Link href="/projects">
-          <button className='p-1 ring-nord9 hover:ring bg-nord10 rounded text-nord4'>Projects</button>
-        </Link>
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <div className="grid place-items-center h-screen">
+        <div className="p-6 max-w-sm mx-auto bg-nord5 rounded-xl shadow-md space-x-4 h-48 ring ring-nord9">
+          <a href={githubProfile}>
+            <Image className='fixed top-1 rounded-full' width='64' height='64' src={profilePicture}></Image>
+          </a>
+          <div className="text-xl font-medium text-nord1">{GithubInfo["name"]}</div>
+          <p className="text-gray-500">{GithubInfo["bio"]}</p>
+          <Link href="/projects">
+            <button className='p-1 ring-nord9 hover:ring bg-nord10 rounded text-nord4'>Projects</button>
+          </Link>
+        </div>
       </div>
     </div>
   )
